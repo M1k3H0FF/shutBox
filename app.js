021d4126b44044330201a0a212c1f7f6134d673e
michaelHoffman <mphoffman8390@gmail.com>
2023-01-27 15:43:16 -0600

    began writing algarithm for game

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-const board = document.getElementById("numbers");
-// const dice = document.getElementById("diceBox");
-board.addEventListener("click", onBoardClick);
-// dice.addEventListener("click", rollDice)
-function onBoardClick(event) {
-  const id = event.target.innerHTML;
-  if (event.target.innerHTML === 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) {
-    event.target.innerHTML = " ";
-  }
-  console.log(id, "line39");
-}
-
 const dice = document.getElementById("dice");
 dice.addEventListener("click", rollDice);
 
-function rollDice(event) {
+function rollDice() {
   setTimeout(function () {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-    console.log(randomNumber1, randomNumber2, "line20");
+    // console.log(randomNumber1, randomNumber2, "line20");
 
     document
       .querySelector(".img1")
@@ -26,5 +14,24 @@ function rollDice(event) {
     document
       .querySelector(".img2")
       .setAttribute("src", "dice" + randomNumber2 + ".png");
+
+    var finalNum = randomNumber1 + randomNumber2;
+    const board = document.getElementById("numbers");
+    board.addEventListener("click", onBoardClick);
+    function onBoardClick(event) {
+      const id = event.target.innerHTML;
+      if(event.target.innerHTML <= finalNum) {
+        event.target.innerHTML = "";
+        var minusNum = finalNum - id;
+        console.log(finalNum, id, minusNum, "line26")
+      }
+      if (event.target.innerHTML <= minusNum) {
+        event.target.innerHTML = "";
+      }
+      console.log(minusNum, "line30")
+     
+      // console.log(finalNum, id, minusNum, "line39");
+    }
+    // console.log(finalNum, id, "line30")
   }, 500);
 }
